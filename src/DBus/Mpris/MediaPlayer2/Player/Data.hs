0b1bc51e4a56a0484da68b318317264803af9740
Matus Goljer <matus.goljer@gmail.com>
2015-12-24 16:10:00 +0100

    Derive Eq on MediaPlayer2.Player data

--- src/DBus/Mpris/MediaPlayer2/Player/Data.hs
+++ src/DBus/Mpris/MediaPlayer2/Player/Data.hs
@@ -7,10 +7,10 @@ module DBus.Mpris.MediaPlayer2.Player.Data
 data PlaybackStatus = Playing -- ^ A track is currently playing.
                     | Paused  -- ^ A track is currently paused.
                     | Stopped -- ^ There is no track currently playing.
-                    deriving (Show, Read)
+                    deriving (Eq, Show, Read)
 
 -- | A repeat / loop status
 data LoopStatus = None     -- ^ The playback will stop when there are no more tracks to play
                 | Track    -- ^ The current track will start again from the begining once it has finished playing
                 | Playlist -- ^ The playback loops through a list of tracks
-                deriving (Show, Read)
+                deriving (Eq, Show, Read)
