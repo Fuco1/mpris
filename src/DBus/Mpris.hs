76bde5dffb54c3a7aae13a013e451109154b0b72
Matus Goljer <matus.goljer@gmail.com>
2015-12-24 16:01:22 +0100

    Add a convenient "main" library entry point.

---
+++ src/DBus/Mpris.hs
@@ -0,0 +1,9 @@
+module DBus.Mpris
+       ( module DBus.Mpris.Monad
+       , module DBus.Mpris.MediaPlayer2
+       , module DBus.Mpris.MediaPlayer2.Player
+       ) where
+
+import DBus.Mpris.Monad
+import DBus.Mpris.MediaPlayer2
+import DBus.Mpris.MediaPlayer2.Player
